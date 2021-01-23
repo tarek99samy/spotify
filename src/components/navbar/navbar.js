d7ed9110249f5cdb8baf6478488ca8c1ea781997
tarek99samy <tarek.saad99@eng-st.cu.edu.eg>
2021-01-23 06:10:43 +0200

    improve responsive design

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -7,138 +7,137 @@ import navbarPhoto from "../../assets/navbarPhoto.png";
 import "./navbar.css";
 
 export default class Navbar extends Component {
-  static contextType = authContext;
-  constructor(props) {
-    super(props);
-    this.state = {
-      isSidebarOpen: false,
-      isDropdownOpen: false,
-    };
+	static contextType = authContext;
+	constructor(props) {
+		super(props);
+		this.state = {
+			isSidebarOpen: false,
+			isDropdownOpen: false,
+		};
 
-    this.toggleSidebar = this.toggleSidebar.bind(this);
-    this.toggleProfileDropdown = this.toggleProfileDropdown.bind(this);
-  }
+		this.toggleSidebar = this.toggleSidebar.bind(this);
+		this.toggleProfileDropdown = this.toggleProfileDropdown.bind(this);
+	}
 
-  toggleSidebar() {
-    this.setState((state) => ({
-      isSidebarOpen: !state.isSidebarOpen,
-    }));
-  }
+	toggleSidebar() {
+		this.setState((state) => ({
+			isSidebarOpen: !state.isSidebarOpen,
+		}));
+	}
 
-  toggleProfileDropdown() {
-    this.setState((state) => ({
-      isDropdownOpen: !state.isDropdownOpen,
-    }));
-  }
+	toggleProfileDropdown() {
+		this.setState((state) => ({
+			isDropdownOpen: !state.isDropdownOpen,
+		}));
+	}
 
-  render() {
-    return (
-      <header className="header">
-        <div className="subheader">
-          <div className="logo-div">
-            <Link to="/">
-              <img src={logo} className="logo" alt="Spotify" />
-            </Link>
-          </div>
+	render() {
+		return (
+			<header className='header'>
+				<div className='subheader'>
+					<div className='logo-div'>
+						<Link to='/'>
+							<img src={logo} className='logo' alt='Spotify' />
+						</Link>
+					</div>
 
-          <nav>
-            <ul className="nav-list">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Premium
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Support
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Download
-                </Link>
-              </li>
-              <li role="separator" className="verticalSeparator"></li>
-              <li>
-                <button
-                  className="nav-button"
-                  onClick={this.toggleProfileDropdown}>
-                  <img src={navbarPhoto} alt="user" className="nav-photo" />
-                  <span>profile</span>
-                </button>
-                {this.state.isDropdownOpen ? (
-                  <div id="profile-dropdown-menu" className="profile-menu">
-                    <ul className="menu">
-                      <li className="nav-item">
-                        <Link to="/" className="profile-item">
-                          Account
-                        </Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link to="/" className="profile-item">
-                          Log out
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
-                ) : null}
-              </li>
-            </ul>
-          </nav>
+					<nav>
+						<ul className='nav-list'>
+							<li className='nav-item'>
+								<Link className='nav-link' to='/'>
+									Premium
+								</Link>
+							</li>
+							<li className='nav-item'>
+								<Link className='nav-link' to='/'>
+									Support
+								</Link>
+							</li>
+							<li className='nav-item'>
+								<Link className='nav-link' to='/'>
+									Download
+								</Link>
+							</li>
+							<li role='separator' className='verticalSeparator'></li>
+							<li>
+								<button className='nav-button' onClick={this.toggleProfileDropdown}>
+									<img src={navbarPhoto} alt='user' className='nav-photo' />
+									<span>
+										profile
+										{this.state.isDropdownOpen ? (
+											<span className='dropdown-icon'>&#8743;</span>
+										) : (
+											<span className='dropdown-icon'>&#8744;</span>
+										)}
+									</span>
+								</button>
+								{this.state.isDropdownOpen ? (
+									<div id='profile-dropdown-menu' className='profile-menu'>
+										<ul className='menu'>
+											<li className='menu-item'>
+												<Link to='/' className='profile-item'>
+													Account
+												</Link>
+											</li>
+											<li className='menu-item'>
+												<Link to='/' className='profile-item'>
+													Log out
+												</Link>
+											</li>
+										</ul>
+									</div>
+								) : null}
+							</li>
+						</ul>
+					</nav>
 
-          <div id="responsive-replacement">
-            <Link to="/" className="nav-link">
-              <img src={navbarPhoto} alt="user" className="nav-photo" />
-            </Link>
+					<div id='responsive-replacement'>
+						<Link to='/' className='nav-link'>
+							<img src={navbarPhoto} alt='user' className='nav-photo' />
+						</Link>
 
-            <button className="nav-button" onClick={this.toggleSidebar}>
-              <span className="open-sidebar-button">&#9776;</span>
-            </button>
-          </div>
-        </div>
+						<button className='nav-button' onClick={this.toggleSidebar}>
+							<span className='open-sidebar-button'>&#9776;</span>
+						</button>
+					</div>
+				</div>
 
-        {this.state.isSidebarOpen ? (
-          <div className="responsive-overlay"></div>
-        ) : null}
+				{this.state.isSidebarOpen ? <div className='responsive-overlay'></div> : null}
 
-        <div
-          id="side-nav"
-          className={
-            this.state.isSidebarOpen ? "sidebar-opened" : "sidebar-closed"
-          }>
-          <div className="close-sidebar-button">
-            <span onClick={this.toggleSidebar}>&times;</span>
-          </div>
-          <ul>
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Premium
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Support
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Download
-              </Link>
-            </li>
-            <li role="separator" className="horizontal-separator"></li>
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                Account
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                Log out
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </header>
-    );
-  }
+				<div id='side-nav' className={this.state.isSidebarOpen ? "sidebar-opened" : "sidebar-closed"}>
+					<div className='close-sidebar-button'>
+						<span onClick={this.toggleSidebar}>&times;</span>
+					</div>
+					<ul>
+						<li className='nav-item'>
+							<Link className='nav-link' to='/'>
+								Premium
+							</Link>
+						</li>
+						<li className='nav-item'>
+							<Link className='nav-link' to='/'>
+								Support
+							</Link>
+						</li>
+						<li className='nav-item'>
+							<Link className='nav-link' to='/'>
+								Download
+							</Link>
+						</li>
+						<li role='separator' className='horizontal-separator'></li>
+						<li className='nav-item'>
+							<Link to='/' className='nav-link'>
+								Account
+							</Link>
+						</li>
+						<li className='nav-item'>
+							<Link to='/' className='nav-link'>
+								Log out
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</header>
+		);
+	}
 }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Navbar from "../../components/navbar/navbar";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect, Link } from "react-router-dom";
 import "./home.css";
 
 export default class Home extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {};
 	}
 
 	render() {
@@ -14,7 +15,21 @@ export default class Home extends Component {
 				<Navbar />
 				<div className='banner'>
 					<div>
-						<h2 className='banner-text'>Go Premium. Be happy.</h2>
+						<h1 className='banner-text'>Go Premium. Be happy.</h1>
+					</div>
+					<div>
+						<Link to='/'>
+							<button className='btn-green'>Start Free Trial</button>
+						</Link>
+					</div>
+					<div className='banner-terms'>
+						<p className='terms-text'>
+							*
+							<Link to='/' className='terms-link'>
+								Terms and conditions
+							</Link>
+							apply. Open only to users who haven't already tried Premium.
+						</p>
 					</div>
 				</div>
 			</>
